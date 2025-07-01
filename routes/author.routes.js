3d44307ecbabcec5c72ebccd394fd13ddfd1bd3a
MahmoudShalabyy <mahmoudelsayedshalabyy4.com>
2025-07-01 16:37:40 +0200

    add files author

---
+++ routes/author.routes.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const router = express.Router();
+
+const authenticateUser = require('../middlewares/authenticateUser');
+const authorizeRoles = require('../middlewares/authorizeRoles');
+const authorController = require('../controllers/authorController');
+
+// Validation from utils/validationSchemas.js
+const validate = require('../middlewares/validate');
+const { createBookSchema, updateBookSchema } = require('../utils/validationSchemas');
+
+// Add new book
+router.post(
+  '/books',
+  authenticateUser,
+  authorizeRoles('author'),
+  validate(createBookSchema),          
+  authorController.createBook
+);
+
+// Get my books
+router.get(
+  '/books',
+  authenticateUser,
+  authorizeRoles('author'),
+  authorController.getMyBooks
+);
+
+// Update book
+router.put(
+  '/books/:id',
+  authenticateUser,
+  authorizeRoles('author'),
+  validate(updateBookSchema),          
+  authorController.updateBook
+);
+
+// Delete book
+router.delete(
+  '/books/:id',
+  authenticateUser,
+  authorizeRoles('author'),
+  authorController.deleteBook
+);
+
+module.exports = router;
